90d1e0b2b41f4a5be6825515eff323d312120273
ericchuk <chukwuaniezeeric@gmail.com>
2024-02-05 16:50:42 -0800

    first task more to come

---
+++ components/welcomePage.js
@@ -0,0 +1,33 @@
+import { StyleSheet, View, Image, Text } from "react-native";
+
+export default function WelcomePage() {
+  return (
+    <View style={style.welcome}>
+      <Image
+        source={
+        require('../assets/Vector.png')
+        }
+        style={{ width: 200, height: 200 }}
+      />
+      <Text style={style.logoText}>Foodu</Text>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  welcome: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  logoText: {
+    fontSize: 40,
+    fontWeight: "bold",
+    textTransform:"uppercase",
+    color: "#000",
+    textAlign: "center",
+    margin: 10,
+    fontFamily: "sans-serif-light"
+  }
+});
